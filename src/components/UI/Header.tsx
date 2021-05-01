dc65ff06597c506338045a2c048c9f2f7f263d90
Javier Eche <javier.eche1205@gmail.com>
2021-05-01 09:50:42 -0500

    Implement header without styles

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,9 +1,16 @@
-function Header(){
+import { ImPhone } from 'react-icons/im';
+import urlLogo from '../../assets/images/logo.svg';
+
+function Header(): JSX.Element{
 
   return (
-    <>
-      
-    </>
+    <div>
+      <img src={urlLogo} alt=""/>
+      <div>
+        <ImPhone />
+        <p>Llámanos</p>
+      </div>
+    </div>
   );
   
 }
