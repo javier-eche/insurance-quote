502f4914c31c89fce7250e8c0cd497033aa8907f
Javier Eche <javier.eche1205@gmail.com>
2021-05-01 18:12:36 -0500

    Created form into Home component and applied some styles

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -11,7 +11,23 @@ function Home(): JSX.Element{
         <div className="home-content-hero">
           <Hero />
         </div>
-        <div className="home-content-form"></div>
+        <div className="home-content-form">
+          <div className="home-content-form-title">
+            <h1>Déjanos tus datos</h1>
+          </div>
+          <form className="home-form">
+            <input className="home-form-input" type="text" placeholder="Nro de doc"/>
+            <input className="home-form-input" type="text" placeholder="Celular" />
+            <input className="home-form-input" type="text" placeholder="Placa" />
+            <div className="home-form-checkbox">
+              <input type="checkbox" name="accept" id="accept" />
+              <label htmlFor="accept">
+                Acepto la Política de Protecciòn de Datos Personales y los Términos y Condiciones.
+              </label>              
+            </div>
+            <button className="home-form-btn-submit">COTIZALO</button>
+          </form>
+        </div>
       </div>
     </div>
   );
