6cbe0b7acf5ad8ebb3a46f75054074c3acb710f9
Javier Eche <javier.eche1205@gmail.com>
2021-05-01 20:05:36 -0500

    Installed React-router-dom and inplemented routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
 import Home from './components/pages/Home';
 import './assets/styles/main.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import InputData from './components/pages/InputData';
 
 function App(): JSX.Element {
   return (
     <>
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/enter-data" component={InputData} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
     </>
   );
 }
